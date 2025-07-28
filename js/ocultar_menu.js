24d98bb4d317cb6f51ddba386b3d062c0b9688e0
10GOMENDOZA <tmeza5839@gmail.com>
2025-07-28 14:50:28 -0500

    eliminacion dea archivo css sin utilizar y cambios en el responsive

--- js/ocultar_menu.js
+++ js/ocultar_menu.js
@@ -2,3 +2,11 @@ function toggleMenu() {
     const menu = document.getElementById('navbar-links');
     menu.classList.toggle('show');
 }navbar-links
+
+
+function Desaparecerfoto () {
+const menu = document.getElementById("menu");
+menu.classList.toggle("hidden");
+menu.classList.toggle("active");
+
+}
